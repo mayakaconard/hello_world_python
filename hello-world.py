68ee8bd5882b1c7edf70a408567edf4b3a2f3a65
mayakaconard <mayakaconard@gmail.com>
2018-09-08 23:46:39 +0300

    Initial Commit

---
+++ hello-world.py
@@ -0,0 +1,5 @@
+print('This is My first program in python')
+
+person=input('Please enter your name')
+print('Hello, ',person)
+print('Dear ', person ,'Thank you for using my application. Wlcome to python')
